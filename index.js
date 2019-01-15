3334d60df149637d99f5f99b70c67b4846a922ac
Christian Jehoshaphat Suryo <christianjehoshaphat@gmail.com>
2019-01-15 13:47:19 +0700

    restyling list

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ const GetDataPromise = () => {
       const array = data.forEach(item => {
         console.log(`Name: ${item.login}`);
         document.getElementById("list").innerHTML += `
-        <div class="card" style="display: flex; flex-direction: column; margin:10px; align-items: center;">
-      <img class="card-img-top" style="width: 9rem;" src="${
-        item.avatar_url
-      }" alt="Avatar" />
+        <div class="container" style="margin: 120px 10px;">
+  <div class="row">
+    <div
+      class="card col"
+    >
+      <img
+        class="card-img-top"
+        style="size: auto;"
+        src="${item.avatar_url}"
+        alt="Avatar"
+      />
       <div class="card-body">
         <h5 class="card-title">${item.login}</h5>
         <a href="${item.html_url}" class="btn btn-primary">Go to Profile</a>
       </div>
     </div>
+  </div>
+</div>
+
         `;
       });
     });
